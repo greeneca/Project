32334aa8c72c52f1a2bc2f08ac39e1211c155bf6
Charlie Greene <088857g@acadiau.ca>
2012-03-19 15:28:53 -0300

    Setup question script

--- Assets/Boatswain Call/Scripts/BoatswainCallQuestions.cs
+++ Assets/Boatswain Call/Scripts/BoatswainCallQuestions.cs
@@ -11,6 +11,11 @@ public class BoatswainCallQuestions : MonoBehaviour {
 	public Rect button2;
 	public Rect button3;
 	public Rect button4;
+	private Rect textNorm;
+	private Rect button1Norm;
+	private Rect button2Norm;
+	private Rect button3Norm;
+	private Rect button4Norm;
 	Rect playArea;
 	int questionNumber;
 	readonly int numberOfQuestions = 2;
@@ -19,11 +24,23 @@ public class BoatswainCallQuestions : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		playArea = new Rect(0, 0, Screen.width, Screen.height);
+		Debug.Log(playArea);
 		questionNumber = 0;
 		questions = new bool[numberOfQuestions];
 		for(int i = 0; i < numberOfQuestions; i++){
 			questions[i] = true;
 		}
+		textNorm = new Rect(text.x * playArea.width, text.y * playArea.height, text.width * playArea.width, text.height * playArea.height);
+		Debug.Log(textNorm);
+		button1Norm = new Rect(button1.x * playArea.width, button1.y * playArea.height, button1.width * playArea.width, button1.height * playArea.height);
+		Debug.Log(button1Norm);
+		button2Norm = new Rect(button2.x * playArea.width, button2.y * playArea.height, button2.width * playArea.width, button2.height * playArea.height);
+		Debug.Log(button2Norm);
+		button3Norm = new Rect(button3.x * playArea.width, button3.y * playArea.height, button3.width * playArea.width, button3.height * playArea.height);
+		Debug.Log(button3Norm);
+		button4Norm = new Rect(button4.x * playArea.width, button4.y * playArea.height, button4.width * playArea.width, button4.height * playArea.height);
+		Debug.Log(button4Norm);
+		
 	}
 	
 	// Update is called once per frame
@@ -35,7 +52,7 @@ public class BoatswainCallQuestions : MonoBehaviour {
  		GUI.skin = menuSkin; 
 		switch(questionNumber){
 		case 0:
-			Question("Test Question", "One", "Two", "Three", "Four", 1);
+			Question("Test Questionadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadadada", "One", "Two", "Three", "Four", 1);
 			break;
 		case 1:
 			Question("Test Question 2", "One", "Two", "Three", "Four", 2);
@@ -48,29 +65,29 @@ public class BoatswainCallQuestions : MonoBehaviour {
 	
 	void Question(string question, string one, string two, string three, string four, int correct){
 		GUI.BeginGroup(playArea);
-		GUI.Label(new Rect(text), question);
-		if(GUI.Button(new Rect(button1), one)){
+		GUI.Label(new Rect(textNorm), question);
+		if(GUI.Button(new Rect(button1Norm), one)){
 			if(correct == 1)
 				questions[questionNumber] = true;
 			else
 				questions[questionNumber] = false;
 			questionNumber++;
 		}
-		if(GUI.Button(new Rect(button2), two)){
+		if(GUI.Button(new Rect(button2Norm), two)){
 			if(correct == 2)
 				questions[questionNumber] = true;
 			else
 				questions[questionNumber] = false;
 			questionNumber++;
 		}
-		if(GUI.Button(new Rect(button3), three)){
+		if(GUI.Button(new Rect(button3Norm), three)){
 			if(correct == 3)
 				questions[questionNumber] = true;
 			else
 				questions[questionNumber] = false;
 			questionNumber++;
 		}
-		if(GUI.Button(new Rect(button4), four)){
+		if(GUI.Button(new Rect(button4Norm), four)){
 			if(correct == 4)
 				questions[questionNumber] = true;
 			else
