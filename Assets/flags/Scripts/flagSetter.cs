db871d5338a341e067040eb8c1d29cb0f58b819e
Thomas Symonds <090911s@acadiau.ca>
2012-03-19 20:19:02 -0300

    fixed stuff

--- Assets/flags/Scripts/flagSetter.cs
+++ Assets/flags/Scripts/flagSetter.cs
@@ -5,33 +5,59 @@ public class flagSetter : MonoBehaviour {
 	
 	// HUD
 	public Texture2D[] flags;
-	public GUITexture flagGUI;
+	//public GUITexture flagGUI;
+	public Texture2D flagGUI;
 	// Use this for initialization
+	public String[] flagnames = {"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", 
+		"Golf", "Hotel", "India", "Juliet", "Kilo", "Lima", "Mike", "November", "Oscar", 
+		"Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", 
+		"Xray", "Yankee", "Zulu"};
+	
+		public String[] answers = {"", "", "", ""};
+	
 	
 	static System.Random r = new System.Random();
 	
+	public Rect menuArea;
+    void buildQuestion()
+	{
+		int n = r.Next() % 26;
+	    flagGUI = flags[n];
+        Debug.Log(n);
+	    Debug.Log(flagnames[n]);
+		int x = r.Next() % 4;
+		Debug.Log(x);
+		answers[x] = flagnames[n];
+		Debug.Log(answers[x]);
+		
+	    for (int i = 0; i <= 3; i++)
+		{
+			
+			int k = r.Next() % 26;
+			
+			if(answers[i].Equals(""))
+			{
+				answers[i] = flagnames[k];
+				//Debug.Log(answers[i]);
+				
+			}
+			
+					
+		}
+		for (int b = 0; b <= 3; b++)
+		{
+			Debug.Log("I'm in the last loop" + answers[b]);
+		}
+	}
+	
 	void Start () {
-	int n = r.Next() % 26;
-    Console.WriteLine(n);
+	buildQuestion();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	renderer.material.SetTexture("_MainTex", flagGUI);
 	}
 }
 
 
-/*
-static Random _r = new Random();
-    static void F()
-    {
-	// Use class-level Random so that when this
-	// ... method is called many times, it still has
-	// ... good Randoms.
-	int n = _r.Next();
-	// If this declared a local Random, it would
-	// ... repeat itself.
-	Console.WriteLine(n);
-	
-	*/
