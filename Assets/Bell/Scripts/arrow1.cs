144f15549d06be7e032bf51b969a4eb062877683
Thomas Symonds <090911s@acadiau.ca>
2012-04-03 14:52:40 -0300

    Just need locks fixed

---
+++ Assets/Bell/Scripts/arrow1.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class arrow1: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void DeleteMe(){
+		Destroy(gameObject);
+	}
+}
